ee70f7c00065b14326ebbb16a4d46a03c8701d3b
PGrabowski-9290 <pawel_grabowski99@outlook.com>
2023-07-05 12:02:05 +0200

    udpate table invoicePosition, invoice, remove typo

--- database/create_database_script.sql
+++ database/create_database_script.sql
@@ -21,8 +21,8 @@ CREATE TABLE IF NOT EXISTS public.car
     user_id UUID NOT NULL,
     brand character varying(45) COLLATE pg_catalog."default" NOT NULL,
     model character varying(45) COLLATE pg_catalog."default" NOT NULL,
-    caryear integer NOT NULL,
-    cartype character varying(15) COLLATE pg_catalog."default" NOT NULL,
+    car_year integer NOT NULL,
+    car_type character varying(15) COLLATE pg_catalog."default" NOT NULL,
     engine_type character varying(15) COLLATE pg_catalog."default" NOT NULL,
     engine_size character varying(8) COLLATE pg_catalog."default" NOT NULL,
     odometer integer NOT NULL,
@@ -45,7 +45,7 @@ CREATE TABLE IF NOT EXISTS public.event
     oil boolean NOT NULL DEFAULT FALSE,
     oil_filter boolean NOT NULL DEFAULT FALSE,
     air_filter boolean NOT NULL DEFAULT FALSE,
-    timming_belt_kit boolean NOT NULL DEFAULT FALSE,
+    timing_belt_kit boolean NOT NULL DEFAULT FALSE,
     description text NOT NULL,
     CONSTRAINT event_id_PK PRIMARY KEY (event_id),
     CONSTRAINT car_id_car_FK FOREIGN KEY (car_id) 
@@ -61,6 +61,7 @@ CREATE TABLE IF NOT EXISTS public.invoice
   event_id UUID NOT NULL,
   invoice_date date NOT NULL,
   invoice_number varchar(30) NOT NULL,
+  currency varchar(3) NOT NULL default 'USD',
   last_update timestamp NOT NULL DEFAULT current_timestamp,
   CONSTRAINT invoice_id_pk PRIMARY KEY (id),
   CONSTRAINT user_id_user_fk FOREIGN KEY (user_id)
@@ -78,7 +79,7 @@ CREATE TABLE IF NOT EXISTS public.invoicePosition
   id UUID NOT NULL DEFAULT gen_random_uuid(),
   invoice_id UUID NOT NULL,
   name varchar(100) NOT NULL,
-  price money NOT NULL,
+  price bigint NOT NULL,
   quantity integer NOT NULL,
   CONSTRAINT id_pk PRIMARY KEY (id),
   CONSTRAINT invoice_id_invoice_fk FOREIGN KEY (invoice_id)
