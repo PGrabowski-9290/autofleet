11fcf5f5c37797c48e412a1442e0f3cec41e9d06
PGrabowski-9290 <pawel_grabowski99@outlook.com>
2023-07-17 02:26:41 +0200

    update db schema

--- backend/src/main/resources/schema.sql
+++ backend/src/main/resources/schema.sql
@@ -1,9 +1,3 @@
--- Table: public.users
-
---DROP TABLE IF EXISTS public.users;
-
-
-
 CREATE TABLE IF NOT EXISTS public.user
 (
     user_id UUID NOT NULL DEFAULT gen_random_uuid(),
@@ -13,46 +7,55 @@ CREATE TABLE IF NOT EXISTS public.user
     reftoken character varying(255) COLLATE pg_catalog."default",
     CONSTRAINT users_pk PRIMARY KEY (user_id),
     CONSTRAINT email_uniq UNIQUE (email)
-);
+)
+
+    TABLESPACE pg_default;
+
 
 CREATE TABLE IF NOT EXISTS public.car
 (
-    car_id UUID NOT NULL DEFAULT gen_random_uuid(),
-    user_id UUID NOT NULL,
+    car_id uuid NOT NULL DEFAULT gen_random_uuid(),
+    user_id uuid NOT NULL,
     brand character varying(45) COLLATE pg_catalog."default" NOT NULL,
     model character varying(45) COLLATE pg_catalog."default" NOT NULL,
     car_year integer NOT NULL,
     car_type character varying(15) COLLATE pg_catalog."default" NOT NULL,
     engine_type character varying(15) COLLATE pg_catalog."default" NOT NULL,
-    engine_size character varying(8) COLLATE pg_catalog."default" NOT NULL,
+    engine character varying(30) COLLATE pg_catalog."default" NOT NULL,
     odometer integer NOT NULL,
     number_plate character varying(15) COLLATE pg_catalog."default" NOT NULL,
-    last_update timestamp DEFAULT current_timestamp,
+    last_update timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT car_id_pk PRIMARY KEY (car_id),
     CONSTRAINT cars_users FOREIGN KEY (user_id)
-        REFERENCES public.user (user_id) MATCH SIMPLE
+        REFERENCES public."user" (user_id) MATCH SIMPLE
         ON UPDATE SET NULL
         ON DELETE CASCADE
-);
+)
+
+    TABLESPACE pg_default;
+
 
 CREATE TABLE IF NOT EXISTS public.event
 (
-    event_id UUID NOT NULL DEFAULT gen_random_uuid(),
-    car_id UUID NOT NULL,
+    event_id uuid NOT NULL DEFAULT gen_random_uuid(),
+    car_id uuid NOT NULL,
     event_date date NOT NULL,
-    last_update timestamp DEFAULT current_timestamp,
-    odometer int NOT NULL,
-    oil boolean NOT NULL DEFAULT FALSE,
-    oil_filter boolean NOT NULL DEFAULT FALSE,
-    air_filter boolean NOT NULL DEFAULT FALSE,
-    timing_belt_kit boolean NOT NULL DEFAULT FALSE,
-    description text NOT NULL,
-    CONSTRAINT event_id_PK PRIMARY KEY (event_id),
-    CONSTRAINT car_id_car_FK FOREIGN KEY (car_id) 
-        REFERENCES public.car (car_id) MATCH SIMPLE 
+    last_update timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+    odometer integer NOT NULL,
+    oil boolean NOT NULL DEFAULT false,
+    oil_filter boolean NOT NULL DEFAULT false,
+    air_filter boolean NOT NULL DEFAULT false,
+    timing_belt_kit boolean NOT NULL DEFAULT false,
+    description text COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT event_id_pk PRIMARY KEY (event_id),
+    CONSTRAINT car_id_car_fk FOREIGN KEY (car_id)
+        REFERENCES public.car (car_id) MATCH SIMPLE
         ON UPDATE CASCADE
         ON DELETE CASCADE
-);
+)
+
+    TABLESPACE pg_default;
+
 
 CREATE TABLE IF NOT EXISTS public.invoice
 (
@@ -67,25 +70,27 @@ CREATE TABLE IF NOT EXISTS public.invoice
   CONSTRAINT user_id_user_fk FOREIGN KEY (user_id)
       REFERENCES public.user (user_id) MATCH SIMPLE
       ON UPDATE NO ACTION
-      ON DELETE CASCADE, 
+      ON DELETE CASCADE,
   CONSTRAINT event_id_event_fk FOREIGN KEY (event_id)
       REFERENCES public.event (event_id) MATCH SIMPLE
       ON UPDATE NO ACTION
       ON DELETE CASCADE
-);
+)
 
-CREATE TABLE IF NOT EXISTS public.invoicePosition
+    TABLESPACE pg_default;
+
+CREATE TABLE IF NOT EXISTS public.invoiceposition
 (
-  id UUID NOT NULL DEFAULT gen_random_uuid(),
-  invoice_id UUID NOT NULL,
-  name varchar(100) NOT NULL,
-  price bigint NOT NULL,
-  quantity integer NOT NULL,
-  CONSTRAINT id_pk PRIMARY KEY (id),
-  CONSTRAINT invoice_id_invoice_fk FOREIGN KEY (invoice_id)
-      REFERENCES public.invoice (id) MATCH SIMPLE
-      ON UPDATE NO ACTION
-      ON DELETE CASCADE
-);
+    id uuid NOT NULL DEFAULT gen_random_uuid(),
+    invoice_id uuid NOT NULL,
+    name character varying(150) COLLATE pg_catalog."default" NOT NULL,
+    price numeric(20,2) NOT NULL,
+    quantity numeric(20,2) NOT NULL,
+    CONSTRAINT id_pk PRIMARY KEY (id),
+    CONSTRAINT invoice_id_invoice_fk FOREIGN KEY (invoice_id)
+        REFERENCES public.invoice (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE CASCADE
+)
 
---stworzenie widoku na podstawie tych tabel z kolumną total podliczającą wartość 
+    TABLESPACE pg_default;
