b7127b349a45849a8e86c96490ad9ebbe56fd76c
PGrabowski-9290 <pawel_grabowski99@outlook.com>
2023-08-10 17:39:20 +0200

    add request validations

--- backend/src/main/java/com/paweu/autofleet/auth/request/RequestLogin.java
+++ backend/src/main/java/com/paweu/autofleet/auth/request/RequestLogin.java
@@ -1,11 +1,13 @@
 package com.paweu.autofleet.auth.request;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
 public record RequestLogin(
-        @NotNull(message = "Email is required") @NotEmpty(message = "Brand is required")
+        @NotNull(message = "Email is required") @NotEmpty(message = "Brand is required") @Email(message = "Invalid email format")
         String email,
-        @NotNull(message = "Password is required") @NotEmpty(message = "Password is required")
+        @NotNull(message = "Password is required") @NotEmpty(message = "Password is required") @Pattern(message = "Password net meet the requirements", regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d!@#$%^&*]{8,}$")
         String password) {
 }
--- backend/src/main/java/com/paweu/autofleet/invoice/request/RequestInvoice.java
+++ backend/src/main/java/com/paweu/autofleet/invoice/request/RequestInvoice.java
@@ -1,14 +1,21 @@
 package com.paweu.autofleet.invoice.request;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
 public record RequestInvoice(
         UUID eventId,
+        @NotNull(message = "Date field is required")
         LocalDate date,
+        @NotNull(message = "Invoice number field is required")
         String invoiceNumber,
+        @NotNull(message = "Currency field is required")
         String currency,
+        @Size(min = 1, message = "At least one position should be provided")
         List<RequestInvoicePos> invoicePosList
 ) {
 }
